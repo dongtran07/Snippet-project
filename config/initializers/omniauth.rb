568dd29c0f5b045e2ad31bb33bfbeba81cd9d12c
dongtran07 <dong.tm1212@gmail.com>
2014-06-02 17:07:01 +0700

    fix private edit page

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '452617783774-1uj2n9mgfsvn0fb9cadsb15cb63g0tjp.apps.googleusercontent.com', 'hBQIMfclog1nSOGJ2F1O51hy', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '452617783774-6mr76gcfdff53fsldfqd9690rv4vmjko.apps.googleusercontent.com', 'hIDap15DkHdBttlTBValf_oV', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
